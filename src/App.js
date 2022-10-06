73098ab77d4c18b9968bbbb17c366e5ba24c4485
MunibQazi12 <munibqazi770@gmail.com>
2022-10-06 17:11:02 +0500

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return(
+      <div className="text-3xl font-bold underline">
+        <h1> Hello, World! </h1>
+      </div>
+    );
+}
+
+export default App;
