a0ba141e275c187ff341daead0641df25771d25c
MunibQazi <munib@dataltics.com>
2022-11-28 19:54:10 +0500

    Header component added into the admin panel

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,43 @@
+import '../../assets/styles/header.css';
+import MenuBars from '../../assets/images/menu-bars.png';
+import Search from '../../assets/images/search.png';
+import NotificationBell from '../../assets/images/notification-bell.png';
+import Acitivity from '../../assets/images/activity.png';
+import Profile from '../../assets/images/profile.png';
+
 const Header = () => {
     return (
         <>
-            <h1>Header</h1>
+            <div class="flex w-full">
+                <div class="body-content-right">
+                    <header class="bg-white border-b md:px-8 px-4">
+                        <div class="flex justify-between items-center w-full">
+                            <form class="w-10/12 py-2.5 sm:block flex items-center">
+                                <div class="sm:hidden mr-4">
+                                    <button class="w-6 h-6"  onclick="sidebarmob()">
+                                        <img src={MenuBars} alt="Menu Bars" />
+                                    </button>
+                                </div>
+                                <div class="flex items-center  relative">
+                                    <img class="absolute left-0" src={Search} alt="search" />
+                                    <input class="text-sm font-normal leading-3 py-2.5 px-3 ml-6 w-full text-757575" type="text" name="search" placeholder="Search" />
+                                </div>
+                            </form>
+                            <div class="flex items-center py-4 border-l">
+                                <button class="w-6 h-6 ml-5" type='button'>
+                                    <img src={NotificationBell} alt="notification bell" />
+                                </button>
+                                <button class="w-6 h-6 sm:ml-9 ml-5" type='button'>
+                                    <img src={Acitivity} alt='activity' />
+                                </button>
+                                <button class="w-6 h-6 sm:ml-9 ml-5" type='button'>
+                                    <img src={Profile} alt='profile' />
+                                </button>
+                            </div>
+                        </div>
+                    </header>
+                </div>
+            </div>
         </>
     )
 }
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -37,122 +37,133 @@ const Sidebar = () => {
             </div>
             <div className="flex flex-col justify-between h-full px-2.5 pt-4 pb-6 bg-fafafa">
                 <div>
-                    <form>
-                        <div className="lg:border sm:border-0 border rounded lg:bg-white sm:bg-transparent bg-white flex justify-between items-center pl-3 pr-3.5 py-2 relative">
-                            <img className="absolute left-3 w-3 h-3" src={Input} alt="Search" />
-                            <input className="fs-13 font-normal leading-3 lg:block sm:hidden text-757575 ml-5 h-5 w-3/4 p-2" type="text" name="search" placeholder="Jump to" />
-                            <span className="w-6 h-4 justify-center items-center lg:flex sm:hidden rounded-sm bg-e0e0e0">
-                                <img className="mx-auto" src={XS} alt="X-S" />
-                            </span>
+                    <div className='border-b pb-2'>
+                        <form>
+                            <div className="lg:border sm:border-0 border rounded lg:bg-white sm:bg-transparent bg-white flex justify-between items-center pl-3 pr-3.5 py-2 relative">
+                                <img className="absolute left-3 w-3 h-3" src={Input} alt="Search" />
+                                <input className="fs-13 font-normal leading-3 lg:block sm:hidden text-757575 ml-5 h-5 w-3/4 p-2" type="text" name="search" placeholder="Jump to" />
+                                <span className="w-6 h-4 justify-center items-center lg:flex sm:hidden rounded-sm bg-e0e0e0">
+                                    <img className="mx-auto" src={XS} alt="X-S" />
+                                </span>
+                            </div>
+                        </form>
+                        <div>
+                            <a className="flex justify-between items-center rounded py-2.5 px-3.5 mt-6 group hover:bg-[#3575d5]">
+                                <div className="flex items-center">
+                                    <span>
+                                        <img className="w-4 h-4 group-hover:hidden" src={Box} alt="Box" />
+                                        <img className="w-4 h-4 group-hover:block hidden" src={BoxWhite} alt="Box" />
+                                    </span>
+                                    <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">Dashboard</span>
+                                </div>
+                                <span className="w-6 h-4 justify-center items-center lg:flex sm:hidden rounded-sm bg-e0e0e0">
+                                    <img className="mx-auto" src={XD} alt="X-D" />
+                                </span>
+                            </a>
                         </div>
-                    </form>
-                    <div className="border-b pb-2">
-                        <a className="flex justify-between items-center rounded py-2.5 px-3.5 mt-6 group hover:bg-[#3575d5]">
+                    </div>
+                    <div className='border-b pb-2'>
+                        <a className="flex justify-between items-center rounded sidebar-dropdown py-2.5 px-3.5 mt-2.5 group hover:bg-[#3575d5]">
                             <div className="flex items-center">
                                 <span>
-                                    <img className="w-4 h-4 group-hover:hidden" src={Box} alt="Box" />
-                                    <img className="w-4 h-4 group-hover:block hidden" src={BoxWhite} alt="Box" />
+                                    <img className="w-4 h-4 group-hover:hidden" src={Union} alt="Union" />
+                                    <img className="w-4 h-4 group-hover:block hidden" src={UnionWhite} alt="union" />
                                 </span>
-                                <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">Dashboard</span>
+                                <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">Campaigns</span>
                             </div>
                             <span className="w-6 h-4 justify-center items-center lg:flex sm:hidden rounded-sm bg-e0e0e0">
-                                <img className="mx-auto" src={XD} alt="X-D" />
+                                <img className="mx-auto" src={XD} />
                             </span>
                         </a>
-                    </div>
-                    <a className="flex justify-between items-center rounded sidebar-dropdown py-2.5 px-3.5 mt-2.5 group hover:bg-[#3575d5]">
-                        <div className="flex items-center">
-                            <span>
-                                <img className="w-4 h-4 group-hover:hidden" src={Union} alt="Union" />
-                                <img className="w-4 h-4 group-hover:block hidden" src={UnionWhite} alt="union" />
-                            </span>
-                            <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">Campaigns</span>
+                        <div>
+                            <a className="flex justify-between items-center rounded py-3 px-3.5 mt-3 group hover:bg-[#3575d5]">
+                                <div className="flex items-center">
+                                    <span>
+                                        <img className="group-hover:hidden w-4 h-4" src={Hierarchy} alt="Share" />
+                                        <img className="w-4 h-4 group-hover:block hidden" src={HierarchyWhite} alt="Share" />
+                                    </span>
+                                    <span className="fs-13 font-medium leading-3 text-616161 lg:block sm:hidden pl-3 sidebar-links group-hover:text-white">WorkFlows</span>
+                                </div>
+                                <span className="w-6 h-4 justify-center items-center lg:flex sm:hidden rounded-sm bg-e0e0e0">
+                                    <img className="mx-auto" src={XD} alt="X-D" />
+                                </span>
+                            </a>
                         </div>
-                        <span className="w-6 h-4 justify-center items-center lg:flex sm:hidden rounded-sm bg-e0e0e0">
-                            <img className="mx-auto" src={XD} />
-                        </span>
-                    </a>
+                    </div>    
                     <div className="border-b pb-2">
-                        <a className="flex justify-between items-center rounded py-3 px-3.5 mt-3 group hover:bg-[#3575d5]">
-                            <div className="flex items-center">
-                                <span>
-                                    <img className="group-hover:hidden w-4 h-4" src={Hierarchy} alt="Share" />
-                                    <img className="w-4 h-4 group-hover:block hidden" src={HierarchyWhite} alt="Share" />
-                                </span>
-                                <span className="fs-13 font-medium leading-3 text-616161 lg:block sm:hidden pl-3 sidebar-links group-hover:text-white">WorkFlows</span>
-                            </div>
-                            <span className="w-6 h-4 justify-center items-center lg:flex sm:hidden rounded-sm bg-e0e0e0">
-                                <img className="mx-auto" src={XD} alt="X-D" />
-                            </span>
-                        </a>
+                        <div>
+                            <a className="flex justify-between items-center rounded py-2.5 px-3.5 mt-3 group hover:bg-[#3575d5]">
+                                <div className="flex items-center">
+                                    <span>
+                                        <img className="group-hover:hidden w-4 h-4" src={Mail} alt="Mail" />
+                                        <img className="w-4 h-4 group-hover:block hidden" src={MailWhite} alt="Mail white" />
+                                    </span>
+                                    <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">E-mail Templates</span>
+                                </div>
+                            </a>
+                        </div>
+                        <div>
+                            <a className="flex justify-between items-center rounded py-2.5 px-3.5 mt-3 sidebar-items group hover:bg-[#3575d5]">
+                                <div className="flex items-center">
+                                    <span>
+                                        <img className="w-4 h-4 group-hover:hidden" src={Cloud} alt="Cloud" />
+                                        <img className="w-4 h-4 group-hover:block hidden"  src={CloudWhite} alt="Cloud" />
+                                    </span>
+                                    <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">Integrations</span>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                    <div>
-                        <a className="flex justify-between items-center rounded py-2.5 px-3.5 mt-3 group hover:bg-[#3575d5]">
-                            <div className="flex items-center">
-                                <span>
-                                    <img className="group-hover:hidden w-4 h-4" src={Mail} alt="Mail" />
-                                    <img className="w-4 h-4 group-hover:block hidden" src={MailWhite} alt="Mail white" />
-                                </span>
-                                <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">E-mail Templates</span>
-                            </div>
-                        </a>
+                    <div className='border-b pb-2'>
+                        <div>
+                            <a className="flex justify-between items-center rounded py-2.5 px-3.5 mt-3 group hover:bg-[#3575d5]">
+                                <div className="flex items-center">
+                                    <span>
+                                        <img className="group-hover:hidden w-4 h-4" src={Target} alt="Target" />
+                                        <img className="w-4 h-4 group-hover:block hidden" src={TargetWhite} alt="Target" />
+                                    </span>
+                                    <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">List & Segments</span>
+                                </div>
+                            </a>
+                        </div>
+                        <div>
+                            <a className="flex justify-between items-center rounded py-2.5 px-3.5 group hover:bg-[#3575d5]">
+                                <div className="flex items-center">
+                                    <span>
+                                        <img className="w-4 h-4 group-hover:hidden" src={User} alt="User" />
+                                        <img className="w-4 h-4 group-hover:block hidden"  src={UserWhite} alt="User" />
+                                    </span>
+                                    <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">Profiles</span>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                    <div className="border-b pb-2">
-                        <a className="flex justify-between items-center rounded py-2.5 px-3.5 mt-3 sidebar-items group hover:bg-[#3575d5]">
-                            <div className="flex items-center">
-                                <span>
-                                    <img className="w-4 h-4 group-hover:hidden" src={Cloud} alt="Cloud" />
-                                    <img className="w-4 h-4 group-hover:block hidden"  src={CloudWhite} alt="Cloud" />
-                                </span>
-                                <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">Integrations</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="">
-                        <a className="flex justify-between items-center rounded py-2.5 px-3.5 mt-3 group hover:bg-[#3575d5]">
-                            <div className="flex items-center">
-                                <span>
-                                    <img className="group-hover:hidden w-4 h-4" src={Target} alt="Target" />
-                                    <img className="w-4 h-4 group-hover:block hidden" src={TargetWhite} alt="Target" />
-                                </span>
-                                <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">List & Segments</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="border-b pb-2">
-                        <a className="flex justify-between items-center rounded py-2.5 px-3.5 group hover:bg-[#3575d5]">
-                            <div className="flex items-center">
-                                <span>
-                                    <img className="w-4 h-4 group-hover:hidden" src={User} alt="User" />
-                                    <img className="w-4 h-4 group-hover:block hidden"  src={UserWhite} alt="User" />
-                                </span>
-                                <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">Profiles</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="">
-                        <a className="flex justify-between items-center rounded py-2.5 px-3.5 group hover:bg-[#3575d5]">
-                            <div className="flex items-center">
-                                <span>
-                                    <img className="group-hover:hidden w-4 h-4" src={Notification} alt="Notification" />
-                                    <img className="w-4 h-4 group-hover:block hidden" src={NotificationWhite} alt="Notification" />
-                                </span>
-                                <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">Pop-Ups</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="border-b pb-2">
-                        <a className="flex justify-between items-center rounded py-2.5 px-3.5 group hover:bg-[#3575d5]">
-                            <div className="flex items-center">
-                                <span>
-                                    <img className="w-4 h-4 group-hover:hidden" src={Network} alt="Network" />
-                                    <img className="w-4 h-4 group-hover:block hidden"  src={NetworkWhite} alt="Network" />
-                                </span>
-                                <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">Data Feeds</span>
-                            </div>
-                        </a>
-                    </div>
+                    <div className='border-b pb-2'>
+                        <div>
+                            <a className="flex justify-between items-center rounded py-2.5 px-3.5 group hover:bg-[#3575d5]">
+                                <div className="flex items-center">
+                                    <span>
+                                        <img className="group-hover:hidden w-4 h-4" src={Notification} alt="Notification" />
+                                        <img className="w-4 h-4 group-hover:block hidden" src={NotificationWhite} alt="Notification" />
+                                    </span>
+                                    <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">Pop-Ups</span>
+                                </div>
+                            </a>
+                        </div>
+                        <div>
+                            <a className="flex justify-between items-center rounded py-2.5 px-3.5 group hover:bg-[#3575d5]">
+                                <div className="flex items-center">
+                                    <span>
+                                        <img className="w-4 h-4 group-hover:hidden" src={Network} alt="Network" />
+                                        <img className="w-4 h-4 group-hover:block hidden"  src={NetworkWhite} alt="Network" />
+                                    </span>
+                                    <span className="fs-13 font-medium leading-3 lg:block sm:hidden text-616161 pl-3 sidebar-links group-hover:text-white">Data Feeds</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>    
                 </div>
+                
                 <div>
                     <div>
                         <a className="flex justify-between items-center rounded py-2.5 px-3.5 mt-3 group hover:bg-[#3575d5]">
